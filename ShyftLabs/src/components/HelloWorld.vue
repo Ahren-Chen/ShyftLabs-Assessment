e0ded060e7cf80b8d43eb6bf6d08aac7cbb7e71e
Ahren Chen <ahren@hotmail.ca>
2024-02-02 15:15:14 -0500

    Added the basics of adding new entries into flask

--- ShyftLabs/src/components/HelloWorld.vue
+++ ShyftLabs/src/components/HelloWorld.vue
@@ -11,10 +11,7 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You've successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+      This is my submission for the ShyftLabs Technical Test</h3>
   </div>
 </template>
 
--- server/app.py
+++ server/app.py
@@ -1,9 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+
 
 students = [
     {
         'first_name': 'John',
+        'family_name': 'Doe',
+        'date_of_birth': '01/01/1990',
+    },
+    {
+        'first_name': 'Jeff',
         'family_name': 'Doe',
         'date_of_birth': '01/01/1990',
     },
@@ -18,12 +24,21 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 
 
 # test route
-@app.route('/students', methods=['GET'])
+@app.route('/students', methods=['GET', 'POST'])
 def all_students():
-    return jsonify({
-        'status': 'success',
-        'students': students
-    })
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        students.append({
+            'first_name': post_data.get('first_name'),
+            'family_name': post_data.get('family_name'),
+            'date_of_birth': post_data.get('date_of_birth'),
+        })
+        response_object['message'] = 'Student added!'
+    else:
+        response_object['students'] = students
+
+    return jsonify(response_object)
 
 
 if __name__ == '__main__':
